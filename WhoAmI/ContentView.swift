d7c6baea13393506c521dcbc207f9ff4be711448
piyushh-11 <115842755+piyushh-11@users.noreply.github.com>
2024-11-08 13:57:28 -0600

    Initial Commit

---
+++ WhoAmI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhoAmI
+//
+//  Created by Piyush Hole on 11/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhoAmI/WhoAmIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoAmIApp.swift
+//  WhoAmI
+//
+//  Created by Piyush Hole on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WhoAmIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
