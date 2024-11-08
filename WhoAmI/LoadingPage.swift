4ed30cd0092f858cc7420b0a54bbacf755ac8a4c
piyushh-11 <115842755+piyushh-11@users.noreply.github.com>
2024-11-08 14:19:16 -0600

    Iniital changes

---
+++ WhoAmI/LoadingPage.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingPage.swift
+//  WhoAmI
+//
+//  Created by Piyush Hole on 11/8/24.
+//
+
+import SwiftUI
+
+struct LoadingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingPage()
+}
