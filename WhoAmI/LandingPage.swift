3d4ccfe9828579aaee1a9134c91c27e1a95563c2
piyushh-11 <115842755+piyushh-11@users.noreply.github.com>
2024-11-08 18:30:10 -0600

    started landing page

---
+++ WhoAmI/LandingPage.swift
@@ -0,0 +1,41 @@
+//
+//  LandingPage.swift
+//  WhoAmI
+//
+//  Created by Piyush Hole on 11/8/24.
+//
+
+import SwiftUI
+
+struct LandingPage: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color(.black)
+                    .ignoresSafeArea()
+                
+                Circle()
+                    .frame(width: 250, height: 250)
+                    .foregroundColor(.accentPurple)
+                    .blur(radius: 100)
+                    .opacity(0.3)
+                
+                Text("WhoAmI?")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                Text("Get Started")
+                    .frame(width: 300, height: 50)
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .background(Color.gray1)
+                    .cornerRadius(25)
+            }
+        }
+    }
+}
+
+#Preview {
+    LandingPage()
+}
--- WhoAmI/WhoAmIApp.swift
+++ WhoAmI/WhoAmIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WhoAmIApp: App {
     var body: some Scene {
         WindowGroup {
-            LoadingPage()
+            LandingPage()
         }
     }
 }
