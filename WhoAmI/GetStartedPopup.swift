0de460837af062defe9b3edd724d7f1b25c9aef4
piyushh-11 <115842755+piyushh-11@users.noreply.github.com>
2024-11-13 10:52:28 -0600

    navigation for popup

--- WhoAmI/GetStartedPopup.swift
+++ WhoAmI/GetStartedPopup.swift
@@ -8,65 +8,80 @@
 import SwiftUI
 
 struct GetStartedPopup: View {
+    
+    @Environment(\.dismiss) var dissmiss
+    
     var body: some View {
         
         GeometryReader { geometry in
-            
-            Rectangle()
-                .fill(Color.gray1)
-                .cornerRadius(35)
-                .frame(height: 500)
-            
-            VStack {
                 
-                HStack {
+            ZStack {
+                Rectangle()
+                    .fill(Color.gray1)
                     
-                    Text("?")
-                        .foregroundColor(.accentPurple)
-                        .font(.system(size: 60))
-                        .fontWeight(.bold)
+                VStack {
+                    
+                    HStack {
+                        
+                        Text("?")
+                            .foregroundColor(.accentPurple)
+                            .font(.system(size: 60))
+                            .fontWeight(.bold)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(Color.gray)
+                            .font(.system(size: 24))
+                            .padding(.top, -20.0)
+                            .onTapGesture {
+                                dissmiss()
+                            }
+                    }
+                    
+                    VStack {
+                        Text("Get Started")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                            .font(.largeTitle)
+                            .frame(maxWidth: 350, alignment: .leading)
+                        Text("Sign up with Phone or Email to start playing today")
+                            .foregroundColor(Color.accentPurple)
+                            .frame(maxWidth: 350, alignment: .leading)
+                            .font(.subheadline)
+                            .padding(.bottom)
+                    }
+                    
+                    Button(action: {
+                        // action
+                    }) {
+                        Text("Continue with Phone")
+                            .frame(width: 350, height: 60)
+                            .font(.title3)
+                            .foregroundColor(Color.white)
+                            .background(Color.gray2)
+                            .cornerRadius(15)
+                            
+                    }
+                    Button(action: {
+                        // action/
+                    }) {
+                        Text("Continue with Email")
+                            .frame(width: 350, height: 60)
+                            .font(.title3)
+                            .foregroundColor(Color.white)
+                            .background(Color.gray2)
+                            .cornerRadius(15)
+                            
+                    }
                     
                     Spacer()
-                    
-                    Image(systemName: "xmark.circle.fill")
-                        .foregroundColor(Color.gray)
-                        .font(.system(size: 24))
-                        .padding(.top, -20.0)
                 }
-                .padding(.bottom)
+                .padding(25)
                 
-                
-                Text("Get Started")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.white)
-                    .font(.title)
-                    .frame(maxWidth: 350, alignment: .leading)
-                Text("Sign up with Phone or Email to start playing today")
-                    .foregroundColor(Color.accentPurple)
-                    .frame(maxWidth: 350, alignment: .leading)
-                    .font(.subheadline)
-                    .padding(.bottom)
-                
-                
-                
-                Text("Continue with Phone")
-                    .foregroundColor(Color.white)
-                    .frame(width: 350, height: 75)
-                    .background(Color.gray2)
-                    .cornerRadius(25)
-                    .font(.title3)
-                Text("Continue with Email")
-                    .foregroundColor(Color.white)
-                    .frame(width: 350, height: 75)
-                    .background(Color.gray2)
-                    .cornerRadius(25)
-                    .font(.title3)
-                                        
             }
-            .padding(25)
-            
+            .ignoresSafeArea()
         }
-            
     }
 }
 
--- WhoAmI/LandingPage.swift
+++ WhoAmI/LandingPage.swift
@@ -8,51 +8,62 @@
 import SwiftUI
 
 struct LandingPage: View {
+    
+    @State private var isPopupPresented: Bool = false
+    
     var body: some View {
-        NavigationView {
-            ZStack {
-                Color(.black)
-                    .ignoresSafeArea()
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            
+            Circle()
+                .frame(width: 250, height: 250)
+                .foregroundColor(.accentPurple)
+                .blur(radius: 100)
+                .opacity(0.3)
+            
+            VStack {
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
                 
-                Circle()
-                    .frame(width: 250, height: 250)
-                    .foregroundColor(.accentPurple)
-                    .blur(radius: 100)
-                    .opacity(0.3)
+                (
+                    Text("WhoAmI")
+                        .foregroundColor(.white)
+                    + Text("?")
+                        .foregroundColor(.accentPurple)
+                )
+                .font(.system(size: 45))
+                .fontWeight(.bold)
                 
-                VStack {
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    
-                    (
-                        Text("WhoAmI")
-                            .foregroundColor(.white)
-                        + Text("?")
-                            .foregroundColor(.accentPurple)
-                    )
-                    .font(.system(size: 45))
-                    .fontWeight(.bold)
-                    
-                    
-                    
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    
+                
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                Button(action: {
+                    isPopupPresented.toggle()
+                }) {
                     Text("Get Started")
                         .frame(width: 300, height: 50)
                         .font(.headline)
                         .foregroundColor(.white)
                         .background(Color.gray1)
                         .cornerRadius(25)
-                    
-                    Spacer()
                 }
+                
+                Spacer()
             }
         }
+        .popover(isPresented: $isPopupPresented, content: {
+            GetStartedPopup()
+                .presentationDetents([.medium])
+        })
+        
     }
+    
 }
 
 #Preview {
